72f48ee762b517a2bfdd638ffff7668b7c51943e
Drewbs <drewbassett24@gmail.com>
2021-01-08 19:16:00 +0000

    wrote a function to track answers and create an array from them

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const startbutton = document.getElementById("start-btn")
+const nextbutton = document.getElementById("next-btn")
 const questionContainerElement = document.getElementById("question-container")
 const questionElement = document.getElementById("question")
 const answerButtonsElement = document.getElementById("answer-buttons")
 
-let shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex //timer goes here
 
 startbutton.addEventListener('click', startGame)
 
 function startGame() {
-    console.log('Started')
+    //console.log('Started')
     startbutton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.floor(Math.random()))
     currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
     setNextQuestion()
+    //call Timer here - Global variable Timer=setInterval () clear it with clearInterval
+    //look at timing events on w3
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
@@ -35,8 +39,19 @@ function showQuestion(question) {
 
 }
 
-function selectAnswer(e) {
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach()
+    setStatusClass9button.datset.correct)
 
 }
 
