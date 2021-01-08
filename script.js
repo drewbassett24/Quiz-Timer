4ad47a47153a4f6da657a0530e2880f98d3f7edd
Drewbs <drewbassett24@gmail.com>
2021-01-08 17:34:13 +0000

    Late commit. Buttons and background created. Begin loops for questions and answers.

---
+++ script.js
@@ -0,0 +1,56 @@
+const startbutton = document.getElementById("start-btn")
+const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerbuttonsElement = document.getElementById("answer-buttons")
+
+let shuffledQuestions, currentQuestionIndex
+
+startbutton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+    startbutton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.floor(Math.random()))
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+
+}
+
+function selectAnswer(e) {
+
+
+}
+
+const questions = [
+    {
+        question: "What is 1?",
+        answers: [
+            { text: "1", correct: true },
+            { text: "2", correct: false },
+            { text: "c", correct: false },
+            { text: "d", correct: false },
+
+        ]
+    }
+
+
+]
